0bf3b8aec42cc9ba561727eb5b10ba1b820fc495
Eduardo Salinas <eduardoluis93@gmail.com>
2024-05-08 20:12:01 +0100

    BUGFIX: I can now move Fang while the Cinemachine camera follows me around. I have a bug in which sometimes, Fang's capsule seems to be twitching. It's more noticeable when you move to the sides (when you strafe).

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -19,6 +19,13 @@ public class PlayerMovement : MonoBehaviour
 	public float jumpForce = 300f;  // This will allow me to jump.
 	private Rigidbody rb;
 
+    // This will help me rotate to turn around (source: PlayerMovementInputController.cs file from Cinemachine tutorial).
+    public Vector2 _look;
+    public float rotationPower = 3f;
+    public GameObject followTransform;
+    // End of the variables for rotating the player based on region (source: PlayerMovementInputController.cs file from Cinemachine tutorial).
+
+
 
     // Start is called before the first frame update
     void Start()
@@ -29,6 +36,38 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        // Another form of rotating the player, which is based by region (source: PlayerMovementInputController.cs file from Cinemachine tutorial).
+        #region Player Based Rotation
+        
+        //Move the player based on the X input on the controller
+        transform.rotation *= Quaternion.AngleAxis(_look.x * rotationPower, Vector3.up);
+
+        #endregion
+
+        #region Vertical Rotation
+        followTransform.transform.rotation *= Quaternion.AngleAxis(_look.y * rotationPower, Vector3.right);
+
+        var angles = followTransform.transform.localEulerAngles;
+        angles.z = 0;
+
+        var angle = followTransform.transform.localEulerAngles.x;
+
+        //Clamp the Up/Down rotation
+        if (angle > 180 && angle < 340)
+        {
+            angles.x = 340;
+        }
+        else if(angle < 180 && angle > 40)
+        {
+            angles.x = 40;
+        }
+
+
+        followTransform.transform.localEulerAngles = angles;
+        #endregion
+        // End of the code for rotating the player based on region (source: PlayerMovementInputController.cs file from Cinemachine tutorial).
+
         float moveHorizontal = Input.GetAxis("Horizontal");
     	float moveVertical = Input.GetAxis("Vertical");
 
@@ -38,18 +77,18 @@ public class PlayerMovement : MonoBehaviour
 
 
 
-       // Check if movement vector is significant to avoid rotating when player is nearly stationary
-       if (movement.magnitude > 0.1f)
-       {
+        //    // Check if movement vector is significant to avoid rotating when player is nearly stationary
+        //    if (movement.magnitude > 0.1f)
+        //    {
 
-           //            // This lets the player rotate, but without causing the camera to shake (source: Copilot).
-           //            movement = Quaternion.Euler(0, Camera.main.transform.eulerAngles.y, 0) * movement;
+        //        //            // This lets the player rotate, but without causing the camera to shake (source: Copilot).
+        //        //            movement = Quaternion.Euler(0, Camera.main.transform.eulerAngles.y, 0) * movement;
 
-           // Create a rotation that looks along the forward vector
-           Quaternion newRotation = Quaternion.LookRotation(movement);
-           // Smoothly rotate towards the new rotation
-           rb.rotation = Quaternion.RotateTowards(rb.rotation, newRotation, rotationSpeed * Time.deltaTime);
-       }
+        //        // Create a rotation that looks along the forward vector
+        //        Quaternion newRotation = Quaternion.LookRotation(movement);
+        //        // Smoothly rotate towards the new rotation
+        //        rb.rotation = Quaternion.RotateTowards(rb.rotation, newRotation, rotationSpeed * Time.deltaTime);
+        //    }
 
         // Move the player. This makes the player move and rotate.
         // BUG: now the player moves way too slowly.
@@ -58,7 +97,11 @@ public class PlayerMovement : MonoBehaviour
         // // This is the old way to move the player. It moves the player quickly, but the player only moves forwards, but never backwards.
     	// transform.Translate(movement * speed * Time.deltaTime);  
 
+        //Set the player rotation based on the look transform (source: PlayerMovementInputController.cs file from Cinemachine tutorial)
+        transform.rotation = Quaternion.Euler(0, followTransform.transform.rotation.eulerAngles.y, 0);
 
+        //reset the y rotation of the look transform (source: PlayerMovementInputController.cs file from Cinemachine tutorial)
+        followTransform.transform.localEulerAngles = new Vector3(angles.x, 0, 0);
 
         // This lets the Player jump while pressing the space bar.
         // BUG: The player can't jump yet.
