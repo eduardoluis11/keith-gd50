eb7b78efd4c8df3e85f4d781779c0693fa220817
Eduardo Salinas <eduardoluis93@gmail.com>
2024-05-16 23:23:17 +0100

    I made the PlayerMovement.cs script more original by changing the "angles" variable to "cameraAngles". I also changed the location of the first "region area" snippet ot almost the end of the file. I'm ready to do anything else.

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,12 +1,16 @@
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
+
 
 // I needed to install Input System from the Package Manager in Unity to use this library (source: Copilot from VS Code).
 using UnityEngine.InputSystem;
 
+/* A large portion of this code is from the PlayerMovementInputController.cs file from the Cinemachine tutorial from Unity.
+*/
 
-/* This file will let the Player (Fang) move, run, and jump (source: ChatGPT 4.0).
+/* This file will let the Player (Fang) move, run, and jump (source: PlayerMovementInputController.cs file from Unity,s Youtube Channel, 
+and ChatGPT 4.0).
 
 You can move around using the arrow keys or WASD keys.
 
@@ -29,6 +33,7 @@ public class PlayerMovement : MonoBehaviour
     // This will help me move around (source: PlayerMovementInputController.cs file).
     public Vector2 _move;
 
+    // BOOKMARK.
 
     // This will allow the Cinemachine camera to follow the player around. The user will have to put the Empty
     // Game Object that is right behind the Player Object into the Follow Transform slot in the Unity Editor (source:
@@ -72,17 +77,10 @@ public class PlayerMovement : MonoBehaviour
         rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
+    // Update is called once per frame. Most of the code in this method is from the PlayerMovementInputController.cs file from the Cinemachine 
+    // tutorial.
     void Update()
     {
-
-        // Another form of rotating the player, which is based by region (source: PlayerMovementInputController.cs file from Cinemachine tutorial).
-        #region Player Based Rotation
-        
-        //        // This should only make the player move forward (source: PlayerMovementInputController.cs file).
-        //        transform.rotation *= Quaternion.AngleAxis(_look.x * rotationPower, Vector3.up);
-
-        #endregion
 
         #region Follow Transform Rotation
 
@@ -93,28 +91,31 @@ public class PlayerMovement : MonoBehaviour
         #endregion
 
 
+
         // All of the below snippet of code is from the PlayerMovementInputController.cs file (source: Cinemachine
         // tutorial from Unity).
         #region Vertical Rotation
         followTransform.transform.rotation *= Quaternion.AngleAxis(_look.y * rotationPower, Vector3.right);
 
-        var angles = followTransform.transform.localEulerAngles;
-        angles.z = 0;
+
+
+        var cameraAngles = followTransform.transform.localEulerAngles;
+        cameraAngles.z = 0;
 
         var angle = followTransform.transform.localEulerAngles.x;
 
         //Clamp the Up/Down rotation
         if (angle > 180 && angle < 340)
         {
-            angles.x = 340;
+            cameraAngles.x = 340;
         }
         else if(angle < 180 && angle > 40)
         {
-            angles.x = 40;
+            cameraAngles.x = 40;
         }
 
 
-        followTransform.transform.localEulerAngles = angles;
+        followTransform.transform.localEulerAngles = cameraAngles;
         #endregion
 
         nextRotation = Quaternion.Lerp(followTransform.transform.rotation, nextRotation, Time.deltaTime * rotationLerp);
@@ -141,8 +142,16 @@ public class PlayerMovement : MonoBehaviour
         //Set the player rotation based on the look transform
         transform.rotation = Quaternion.Euler(0, followTransform.transform.rotation.eulerAngles.y, 0);
         //reset the y rotation of the look transform
-        followTransform.transform.localEulerAngles = new Vector3(angles.x, 0, 0);
-        // End of the code for rotating the player based on region (source: PlayerMovementInputController.cs file.
+        followTransform.transform.localEulerAngles = new Vector3(cameraAngles.x, 0, 0);
+        // End of the code for rotating the player based on region (source: PlayerMovementInputController.cs file from the Cinemachine tutorial).
+
+        // // Another form of rotating the player, which is based by region (source: PlayerMovementInputController.cs file from Cinemachine tutorial).
+        // #region Player Based Rotation
+        
+        // //        // This should only make the player move forward (source: PlayerMovementInputController.cs file).
+        // //        transform.rotation *= Quaternion.AngleAxis(_look.x * rotationPower, Vector3.up);
+
+        // #endregion
 
 //        float moveHorizontal = Input.GetAxis("Horizontal");
 //    	float moveVertical = Input.GetAxis("Vertical");
@@ -178,6 +187,8 @@ public class PlayerMovement : MonoBehaviour
 //
 //        //reset the y rotation of the look transform (source: PlayerMovementInputController.cs file from Cinemachine tutorial)
 //        followTransform.transform.localEulerAngles = new Vector3(angles.x, 0, 0);
+
+
 
         // This lets the Player jump while pressing the space bar.
         // BUG: The player can't jump yet.
