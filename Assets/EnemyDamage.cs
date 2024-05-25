f578979dd71479cf55b11f4df4ad3ae0a75efa8b
Eduardo Salinas <eduardoluis93@gmail.com>
2024-05-25 20:03:02 +0100

    BUGGY: I can receive damage if the Imanus' capsule touches Fang. However, when the enemy capsule touches me, I need to move to get touched, so that I can receive points of damage. Also, I don't receive damage 20 times per second, like I expected. I also fall if the Imanus' capsule touches Fang and Fang moves.

---
+++ Assets/EnemyDamage.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* This script will let the Boss / Enemy to hurt the Player, so that Fang takes damage when he touches the Boss. 
+
+In this script, `damage` is the amount of HP the player will lose when the enemy collides with them. The 
+`OnCollisionEnter` method is a Unity callback that is invoked when this object starts colliding with another object. 
+Inside this method, we first check if the object we collided with is the player (by checking if its tag is "Player"). 
+If it is, we get the `PlayerStats` component from the player object and reduce the player's HP by the enemy's damage
+(source: Copilot).
+
+To use this script, you would attach it to the enemy's capsule. Also, make sure that your player character's game 
+object has the tag "Player" and the `PlayerStats` script attached (source: Copilot).
+
+*/
+
+public class EnemyDamage : MonoBehaviour
+{
+
+    // This is the damage that the Boss will do to the Player (source: Copilot)
+    public int damage = 10;
+
+    // This detects if the Boss makes collision with the Player (source: Copilot)
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            PlayerStats playerStats = collision.gameObject.GetComponent<PlayerStats>();
+            if (playerStats != null)
+            {
+                // If the Boss touches the Player, the Player will take damage
+                playerStats.HP -= damage;
+
+                // DEBUG: This will print "Player took (number of points) damage" in the console
+                Debug.Log("Player took " + damage + " damage");
+
+            }
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+
