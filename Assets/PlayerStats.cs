a011a109656595c71d5770cd81ea5b2a0fed5083
Eduardo Salinas <eduardoluis93@gmail.com>
2024-05-25 20:18:56 +0100

    I can now get a Game Over if I lose all of my HP and Die. Right now, the Imanus deals 100 HP if he touches you once.

--- Assets/PlayerStats.cs
+++ Assets/PlayerStats.cs
@@ -2,10 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// This will let me use the SceneManager class to load the "Game Over" scene (source: VS Code's Copilot)
+using UnityEngine.SceneManagement;
+
 
 /* This script will store the Player Character's stats (such as HP, and Attack and Defense points.)
 
 This is more or less like the "init()" function of the Player Character.
+
+To make the Player get a Game Over if the Player loses all of their HP, you will need to check if the player's HP has
+dropped to 0 in the Update method, and if so, load the "Game Over" scene.  You will need to use
+SceneManager.LoadScene("GameOver"); to load the "Game Over" scene. Make sure you have added the "GameOver" scene to
+your build settings in Unity (source: Copilot).
+
+The Update method checks every frame if the player's HP has dropped to 0 or less. If it has, it loads the "Game Over"
+scene (source: Copilot).
  */
 
 public class PlayerStats : MonoBehaviour
@@ -23,9 +34,15 @@ public class PlayerStats : MonoBehaviour
         
     }
 
-    // Update is called once per frame
+    // Update is called once per frame.
+    // This checks every frame if the player's HP has dropped to 0 or less. If it has, it loads the "Game Over" scene
     void Update()
     {
-        
+        // If the player's HP drops to 0 or less
+        if (HP <= 0)
+        {
+            // Load the "Game Over" scene
+            SceneManager.LoadScene("GameOver");
+        }
     }
 }
