8609d9de3dc1a3475b00c43951d0646a21f789a9
Eduardo Salinas <eduardoluis93@gmail.com>
2024-05-25 19:31:28 +0100

    I created a Script for Storing Fang's Stats (such as HP, Attack, and Defense points.)

---
+++ Assets/PlayerStats.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/* This script will store the Player Character's stats (such as HP, and Attack and Defense points.)
+
+This is more or less like the "init()" function of the Player Character.
+ */
+
+public class PlayerStats : MonoBehaviour
+{
+
+    // These are the stats of the Player Character (source: Pycharm's Copilot)
+    public int HP = 100;    // The Player Character's Health Points
+    public int attack = 10; // The Player Character's Attack Points
+    public int defense = 5; // The Player Character's Defense Points
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
