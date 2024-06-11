48b7bd7c7dea0ec5e72cdb71704bd0bd345ab3c4
Eduardo Salinas <eduardoluis93@gmail.com>
2024-06-11 21:04:59 +0100

    Updated the references.txt file
    I updated the references.txt file to include some of the videos from Brackeys' 'How to make an RPG' tutorial series, and a note saying that, if I need to include all 14 episodes in my references, I will do so.

--- Assets/BrackeysScripts/Controllers/EnemyController.cs
+++ Assets/BrackeysScripts/Controllers/EnemyController.cs
@@ -2,8 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/* This script will handle the enemy's behavior. 
+
+Source of most of this code: Brackeys from https://youtu.be/xppompv1DBg?si=1nNpE-RKdg8whd45
+
+
+*/
+
 public class EnemyController : MonoBehaviour
 {
+
+    public float speed = 10f;
+
     // Start is called before the first frame update
     void Start()
     {
