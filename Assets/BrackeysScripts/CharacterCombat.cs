8d0d0eb2824b15db08e9a31502f28113e99628b2
Eduardo Salinas <eduardoluis93@gmail.com>
2024-06-12 21:10:56 +0100

    BUGGY: Started Ep 11 of Brackeys Tutorial
    I added the 11th episode of Brackeys' How to make an RPG tutorial to my references.txt file, and I added some code from that tutorial into my Test Brackeys scene.

---
+++ Assets/BrackeysScripts/CharacterCombat.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* Script for handling combat.
+
+Source of most of this code: Episode 11 of this tutorial from Brackeys: https://youtu.be/FhAdkLC-mSg?si=6gtY_Thz0OCcBrTg
+*/
+
+[RequireComponent(typeof(CharacterStats))]
+public class CharacterCombat : MonoBehaviour
+{
+
+    public float attackSpeed = 1f;
+    private float attackCooldown = 0f;
+    
+    CharacterStats myStats;
+
+    void Start ()
+    {
+        myStats = GetComponent<CharacterStats>();
+    }
+
+    void Update ()
+    {
+        attackCooldown -= Time.deltaTime;
+    }
+
+    public void Attack (CharacterStats targetStats)
+    {
+        if (attackCooldown <= 0f)
+        {
+            
+            targetStats.TakeDamage(myStats.damage.GetValue());
+            attackCooldown = 1f / attackSpeed;
+        }
+    }
+
+}
--- Assets/BrackeysScripts/Enemy.cs
+++ Assets/BrackeysScripts/Enemy.cs
@@ -8,11 +8,29 @@ Source of most of this code: Brackeys, Episode 10, from https://youtu.be/xppompv
 
 */
 
+[RequireComponent(typeof(CharacterStats))]
 public class Enemy : Interactable
 {
+
+    PlayerManager playerManager;
+    CharacterStats myStats;
+
+    void Start ()
+    {
+        playerManager = PlayerManager.instance;
+        myStats = GetComponent<CharacterStats>();
+
+    }
+
     public override void Interact()
     {
         base.Interact();
+        CharacterCombat playerCombat = playerManager.player.GetComponent<CharacterCombat>();
+        if (playerCombat != null)
+        {
+            playerCombat.Attack(myStats);
+        }
+
         // Attack the enemy
     }
 }
