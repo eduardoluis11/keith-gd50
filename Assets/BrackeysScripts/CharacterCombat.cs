1d4f6397e215f95f6da0b6e0052f800ba9640382
Eduardo Salinas <eduardoluis93@gmail.com>
2024-06-14 19:22:36 +0100

    BUGGY: I finished E11 of Tutorial
    I just finished watching Episode 11 of Brackeys' Tutorial on How to Make an RPG.

--- Assets/BrackeysScripts/CharacterCombat.cs
+++ Assets/BrackeysScripts/CharacterCombat.cs
@@ -13,6 +13,10 @@ public class CharacterCombat : MonoBehaviour
 
     public float attackSpeed = 1f;
     private float attackCooldown = 0f;
+
+    public float attackDelay = .6f;
+
+    public event System.Action OnAttack;
     
     CharacterStats myStats;
 
@@ -31,9 +35,20 @@ public class CharacterCombat : MonoBehaviour
         if (attackCooldown <= 0f)
         {
             
-            targetStats.TakeDamage(myStats.damage.GetValue());
+            StartCoroutine(DoDamage(targetStats, attackDelay));
+
+            if (OnAttack != null)
+                OnAttack();
+
             attackCooldown = 1f / attackSpeed;
         }
+    }
+
+    IEnumerator DoDamage(CharacterStats stats, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+
+        stats.TakeDamage(myStats.damage.GetValue());
     }
 
 }
