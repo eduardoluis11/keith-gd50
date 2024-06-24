3cfe8b18fe8358ee4e6cc5a9a5f07be4c663a014
Eduardo Salinas <eduardoluis93@gmail.com>
2024-06-24 18:34:42 +0100

    Finished Episode 12 Again
    Fang can execute a sword slashing animation, and right after the animation, he enters into the Combat Stance animation. I finished Episode 12 once again.

--- Assets/Scripts/CharacterAnimator.cs
+++ Assets/Scripts/CharacterAnimator.cs
@@ -7,20 +7,36 @@ using UnityEngine.AI;
 
 Source of most of this code: 
 https://youtu.be/COckHIIO8vk?si=txD3BSuwAvNkY4w3
+
+Additionally, this script also has code from this video from Brackeys / Sebastian Lague:
+https://youtu.be/yhPRkihs-Yg?si=W_x3imWDgruUbED5
 */
 
 public class CharacterAnimator : MonoBehaviour {
+
+
+
+	public AnimationClip replaceableAttackAnim;
+	public AnimationClip[] defaultAttackAnimSet;
+	protected AnimationClip[] currentAttackAnimSet;
 
     const float locomationAnimationSmoothTime = .1f;
 
     NavMeshAgent agent;
     protected Animator animator;
 	protected CharacterCombat combat;
+	protected AnimatorOverrideController overrideController;
 
 	protected virtual void Start () {
 		agent = GetComponent<NavMeshAgent>();
 		animator = GetComponentInChildren<Animator>();
 		combat = GetComponent<CharacterCombat>();
+
+		overrideController = new AnimatorOverrideController(animator.runtimeAnimatorController);
+		animator.runtimeAnimatorController = overrideController;
+
+		currentAttackAnimSet = defaultAttackAnimSet;
+		combat.OnAttack += OnAttack;
 	}
 
 	protected virtual void Update () {
@@ -29,4 +45,12 @@ public class CharacterAnimator : MonoBehaviour {
 
 		animator.SetBool("inCombat", combat.InCombat);
 	}
+
+	protected virtual void OnAttack () {
+		animator.SetTrigger("attack");
+		int attackIndex = Random.Range(0, currentAttackAnimSet.Length);
+		// overrideController[replaceableAttackAnim.name] = currentAttackAnimSet[attackIndex];
+		overrideController["Sword Slash 1"] = currentAttackAnimSet[attackIndex];
+	}
+	
 }
--- Assets/Scripts/PlayerAnimator.cs
+++ Assets/Scripts/PlayerAnimator.cs
@@ -5,14 +5,58 @@ using UnityEngine;
 /* This script will handle the player's animations.
 
 Source of most of this code: This video from Brackeys: https://www.youtube.com/watch?v=yhPRkihs-Yg
+
+
+
+Given the structure of WeaponAnimations which contains an array of AnimationClip[] named clips, if you want to transform this into a variable that 
+counts the number of items (i.e., the number of AnimationClip objects within clips), you would typically access the Length property of the array. 
+However, since your goal seems to be to count the number of WeaponAnimations instances (which I assume are collected in an array or list themselves 
+to represent each weapon's animations), you would count the number of WeaponAnimations instances instead (source: VS Code's Copilot).
+
+Assuming you have a collection (like an array or list) of WeaponAnimations, here's how you can set the numberOfWeapons variable:
+
+If you have an array of WeaponAnimations, you use the Length property.
+If you have a list of WeaponAnimations, you use the Count property.
+Here's an example for both cases:
+
+If you have an array of WeaponAnimations:
+If you have a List of WeaponAnimations:
+This numberOfWeapons variable can then be used to define the size of your AnimationClip[][] array if you're planning to organize your animations 
+in a two-dimensional array where each row represents a weapon and each column represents a different animation for that weapon (source: VS Code's Copilot).
+
+Actually, I don't know the exact number of items that I will be storing into the weaponAnimationsArrray. I modified my code  this so that I don't have to type the "numberOfWeapons" variable.
 */
 
 public class PlayerAnimator : CharacterAnimator
 {
+
+    public WeaponAnimations[] weaponAnimations;
+    // This will store the different attack animations
+    // List<List<AnimationClip>> weaponAnimationsList = new List<List<AnimationClip>>();
+    List<List<AnimationClip>> weaponAnimationsList;
+
     // Start is called before the first frame update
     protected override void Start()
     {
         base.Start();
+
+        List<List<AnimationClip>> weaponAnimationsList = new List<List<AnimationClip>>();
+        foreach (WeaponAnimations a in weaponAnimations)
+        {
+            weaponAnimationsList.Add(new List<AnimationClip>(a.clips));
+        }
+
+
+        // AnimationClip[][] weaponAnimationsArray = new AnimationClip[numberOfWeapons][];
+        // weaponAnimationsDict = new Dictionary<AnimationClip[]>();
+        // Dictionary<string, AnimationClip[]> weaponAnimationsDict = new Dictionary<string, AnimationClip[]>();
+    }
+
+    [System.Serializable]
+    public struct WeaponAnimations
+    {
+        
+        public AnimationClip[] clips;
     }
 
     // If I add the "update", the medieval cartoon character won't have any walking nor sprinting animation.
